032df2a59a28fba164adc18379e9a4ccc2c039bc
Henri Heinonen <henriheinonen@Henri-MBP.wlan>
2023-04-18 19:56:39 +0300

    funktioiden alustus/suunnittelu

--- main.c
+++ main.c
@@ -4,10 +4,35 @@
 #include<fcntl.h>
 #include<ncurses.h>
 
+void snake_board();
+void input_check();
+void move_snake();
+void make_fruit();
+
 int main (void) {
     
+    snake_board();
     printf("snake");
 
     return 0;
 }
 
+void snake_board(void)
+{
+  //Tänne koodi, joka tulostaa pelipöydän
+}
+
+void input_check(void)
+{
+  //Täällä chekataan käyttäjän näppäin painallukset
+}
+
+void move_snake(void)
+{
+
+}
+
+void make_fruit(void)
+{
+  //Tänne koodi, jolla tehdään ruokaa pelipöydälle
+}
