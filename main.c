2521e8d50ce68567db44e006bf6241cfb69158eb
Henri Heinonen <henriheinonen@Henri-MBP.wlan>
2023-04-19 11:31:22 +0300

    pelin alustus, käärmeen pää pöydälle

--- main.c
+++ main.c
@@ -4,16 +4,21 @@
 #include<fcntl.h>
 #include<ncurses.h>
 
-int width=50, height=50, gameover=0;
+int head_x, head_y;
+int body_x[50], body_y[50]; //int array käärmeen kropalle, kroppa max koko pelilaudan koko
+int width = 50, height = 50, gameover = 0, size = 1;
 void snake_board();
 void input_check();
 void move_snake();
 void make_fruit();
+void init_game(void); //käärmeen alustus pelipöydälle
 
 int main (void) {
+  init_game();
 
     while(!gameover) //peli loop pyörii, kunnes gameover
     {
+      move_snake();
       snake_board();
       gameover=1;
       
@@ -25,9 +30,9 @@ int main (void) {
 void snake_board(void)
 {
   int i,j,k;
-  for(j=0;j<=height;j++) //ulompi loop kiertää korkeuden verran
+  for(j = 0; j <= height; j++) //ulompi loop kiertää korkeuden verran
   {
-    for(i=0;i<=width;i++) //sisempi loop kiertää leveyden verran
+    for(i = 0; i <= width; i++) //sisempi loop kiertää leveyden verran
     {
       if(i == 0 || j == 0 || j == height || i == width) //rajojen chekkaus
       {
@@ -36,6 +41,10 @@ void snake_board(void)
         {
           printf(" "); //tulostetaan väli
         }
+      }
+      else if(i == head_x && j == head_y)
+      {
+        printf("Q");
       }
       else
       {
@@ -53,8 +62,34 @@ void input_check(void)
   //Täällä chekataan käyttäjän näppäin painallukset
 }
 
+//lisätään käärmeen pää keskelle pelipöytää
+void init_game(void) { 
+    head_x = height / 2;
+    head_y = width / 2;
+}
+
 void move_snake(void)
 {
+  int x1,x2,y1,y2,i;
+  if(size==1)
+  {
+    body_x[0]=head_x; //määritellään pää arrayn ensimmäiseksi
+    body_y[0]=head_y; //myös y suunnassa, kun peli alkaa
+  }
+  else 
+  {
+    x1 = head_x; //pään paikka x suunta
+    y1 = head_y; //pään paikka y suunta
+    for(i = 0; i < size;i++) //pään paikasta kasvatetaan array size verran
+    {
+      x2 = body_x[i];
+      y2 = body_y[i];
+      body_x[i] = x1;
+      body_y[i] = y1;
+      x1 = x2;
+      y1 = y2;
+    }
+  }
 
 }
 
