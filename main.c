e14e2c9f5f1a64bf3c30b7e3cd86a62eb0655577
matiasSuksi <suksimatias@gmail.com>
2023-04-19 15:21:24 +0300

    Handling gameover

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-#include<ctype.h>
 #include<stdlib.h>
-#include<fcntl.h>
 #include<ncurses.h>
 
 int score = 0;
@@ -12,96 +10,73 @@ int body_x[50], body_y[50]; //int array käärmeen kropalle, kroppa max koko pel
 int width = 50, height = 50;
 int gameover = 0, worm_size = 1;
 int dir = 1;
-void snake_board(void);
-void input_check(void);
-char kb_press(void);
-void move_snake(void);
-void make_fruit(void);
-void fruit_collision(void);
-void init_game(void); //käärmeen alustus pelipöydälle
+void snake_board();
+void input_check();
+char kb_press();
+void move_snake();
+void make_fruit();
+void fruit_collision();
+void init_game(); //käärmeen alustus pelipöydälle
+void game();
+void gameover_check();
 
-int main (void) {
-  worm_size = 5;
-  int test_rounds = 4;
+int main ()
+{
     init_game();
     make_fruit();
-    while(!gameover) //peli loop pyörii, kunnes gameover
-    {
-      snake_board();
-      kb_press();
-      input_check();
-      move_snake();
-      fruit_collision();
-      test_rounds--;
-      if(test_rounds<=0){
-        gameover = 1;
-      }
-      
-    }
+    game();
     return 0;
 }
 
-void snake_board(void)
+void game()
 {
-  int i, j, k, no_body;
-  for(j = 0; j <= height; j++) //ulompi loop kiertää korkeuden verran
-  {
-    for(i = 0; i <= width; i++) //sisempi loop kiertää leveyden verran
-    {
-      no_body=1;
-      if(i == 0 || j == 0 || j == height || i == width) //rajojen chekkaus
-      {
-        printf("#"); //tulostetaan pelin seinät peli rajoille
-        if(i == width) // lisää väli jollei ole oikean puoleisessa seinässä
-          {
-            printf(" "); //tulostetaan väli
-          }
-      } 
-      else if(i == fruit_x && j == fruit_y) //tulostetaan hedelmä jos kordinaatit kohtaavat
-      {
-          printf("*"); //hedelmä
-      }
-      else if(i == head_x && j == head_y)
-      {
-          printf("Q"); //madon pää
-      }
-      else if(i == head_x && j == head_y) //mikäli ruutu vastaa pään sijaintia tulosta pää
-      {
-        printf("Q");
-      }
-      else
-      {
-        for(k = 0; k < worm_size; k++) //tulostetaan käärmeen kroppa
-        {
-          if(i == body_x[k] && j == body_y[k]) //mikäli ruutu vastaa kroppaa tulosta kroppa
-          {
-          printf("o"); 
-          no_body = 0; //vaihdetaan ei kroppaa epätodeksi
-          }
-        }
-        if(no_body) //mikäli ei käärmeen kroppa tosi tulostaa tyhjää
-        {
-          printf(" ");
-        }
-        
-      }
-/*       else
-      {
-        printf(" "); //kierroksen lopuksi väli muuten tulostaa vasemman seinän viereen oikean
-      } */
+    while(!gameover) {
+        snake_board();
+        input_check();
+        move_snake();
+        fruit_collision();
+        gameover_check();
     }
-    printf("\n");
-  }
-  fflush(stdout); // tyhjennetään bufferi, jotta uudet kierrokset tulostuu oikein
+    system("cls");
+    printf("Your final Score : %5d", score);
 }
 
+void snake_board(void) {
+    system("cls");
+    int i,j,k,no_body;
+    for(j=0;j<=height;j++) {
+        for(i=0;i<=width;i++) {
+            no_body=1;
+            if(i==0||j==0||j==height||i==width)
+                printf("#");
+            else if(i==fruit_x&&j==fruit_y)
+                printf("*");
+            else if(i==head_x&&j==head_y)
+                printf("Q");
+            else {
+                for(k=0;k<worm_size;k++) {
+                    if(i==body_x[k] && j==body_y[k]) {
+                        printf("+");
+                        no_body=0;
+                    }
+                }
+                if(no_body) {
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
+    printf("Scored : %5d\n",score);
+    fflush(stdout); // Tarvitaan macOS:lle
+}
 /**
 Käsittelee näppäimen painalluksen sekä määrittää mihin suuntaan mato liikkuu.
 Dir-muuttuja on numeerinen muuttuja, joka symboloi madon suuntaa.
 Suunnat ovat 1,2,3 ja 4 eli ylös, oikea, alas ja vasen.
 If-lohkot tarvitaan estämään vastakkaisten liikkeiden samanaikaisuus.
 **/
-void input_check(void)
+void input_check()
 {    
     switch (kb_press()) 
     {
@@ -123,7 +98,7 @@ void input_check(void)
     }
 }
 
-char kb_press(void) {
+char kb_press() {
     initscr();
     timeout(50);
     noecho();
@@ -135,12 +110,12 @@ char kb_press(void) {
 }
 
 //lisätään käärmeen pää keskelle pelipöytää
-void init_game(void) { 
+void init_game() { 
     head_x = height / 2;
     head_y = width / 2;
 }
 
-void move_snake(void)
+void move_snake()
 {
   int x1, x2, y1, y2, i;
   if(worm_size == 1)
@@ -152,7 +127,7 @@ void move_snake(void)
   {
     x1 = head_x; //pään paikka x suunta
     y1 = head_y; //pään paikka y suunta
-    for(i = 0; i < worm_size; i++) //pään paikasta kasvatetaan array worm_size verran
+    for(i = 0; i < worm_size; i++) //pään paikasta kasvatetaan array worm_worm_size verran
     {
       x2 = body_x[i];
       y2 = body_y[i];
@@ -180,17 +155,18 @@ void move_snake(void)
 }
 
 //Luo uusia hedelmiä pelipöydälle
-void make_fruit(void)
+void make_fruit()
 {
-    resetfruit_x: fruit_x = rand() % 10;
+    resetfruit_x: fruit_x = rand() % 20;
     if(fruit_x == 0 || fruit_x == width)
         goto resetfruit_x;
-    resetfruit_y: fruit_y = rand() % 10;
+    resetfruit_y: fruit_y = rand() % 20;
     if(fruit_y == 0 || fruit_y == height)
         goto resetfruit_y; 
 }
 
-void fruit_collision(void) {
+void fruit_collision()
+{
     if(head_x == fruit_x && head_y == fruit_y) {
         score += 5;
         worm_size++;
@@ -200,3 +176,17 @@ void fruit_collision(void) {
     }
 }
 
+void gameover_check(void)
+{
+    int i;
+    for(i=0;i<worm_size;i++)
+    {
+        if(head_x==body_x[i] && head_y==body_y[i])
+        {
+            gameover=1;
+        }
+    if(head_x==width||head_x==0||head_y==height||head_y==0)
+    {
+        gameover=1;
+    }
+}
